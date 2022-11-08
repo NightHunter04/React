c893a5e6975de9f3c66487959f3b0599e5dd1276
Nicolas Dondo <nicodondo1980@gmail.com>
2022-11-08 00:23:06 -0300

    Check Form

--- src/Components/Chequear/Chequear.js
+++ src/Components/Chequear/Chequear.js
@@ -2,13 +2,15 @@ import React from 'react'
 import { db } from '../../firebase/firebase'
 import { collection, } from "firebase/firestore"
 import {Formulario} from "../Formulario/Formulario"
+import { useState } from 'react'
+import { Cart} from "../../Components/Containers/CartView/Cart"
 
 
  const Chequear = () => {
   return (
     <div>
         <h2>
-          Tus datos de compra son:
+          Tus datos de compra son:{}
         </h2>
               </div>
  )
