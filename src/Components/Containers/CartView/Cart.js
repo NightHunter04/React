cf1ab4c7e06d5d8ee28cd6852fecea99b7ec8972
Nicolas Dondo <nicodondo1980@gmail.com>
2022-11-09 01:26:23 -0300

    Ruteo

--- src/Components/Containers/CartView/Cart.js
+++ src/Components/Containers/CartView/Cart.js
@@ -87,7 +87,7 @@ return (
      <div className="detail-cart">
       
            <Formulario/>  
-           <Link to="/"> 
+           <Link to="/"
            <Button className="boton-finalizar" onClick={finalizarCompra} variant="contained" endIcon={<SendIcon />}>Finalizar Compra</Button>
           </Link>
       </div> 
